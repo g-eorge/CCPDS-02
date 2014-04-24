ecfd8b93b40dea4968afee8927e9c57f6462da6f
George Agnelli <george.agnelli@gmail.com>
2014-04-24 23:53:07 +0100

    adds tools for part1b

--- solution/1-billing/etl/load_csv.sh
+++ solution/1-billing/etl/load_csv.sh
@@ -14,7 +14,6 @@ fi
 hadoop fs -mkdir -p $TMP/provider_charge
 
 
-tail -n+2 $LOCALTMP/provider_charge.csv | sed -rn -e 's/^(.+),(.+),"(.+)",(.+),(.+),(.+),(.+),(.+)$/\1\t\2\t\3\t\4\t\5\t\6\t\7\t\8/p' \
-  -e 's/^(.+),(.+),(.+),(.+),(.+),(.+),(.+),(.+)$/\1\t\2\t\3\t\4\t\5\t\6\t\7\t\8/p' | hadoop fs -put - $TMP/provider_charge/provider_charge.txt
+tail -n+2 $LOCALTMP/provider_charge.tsv | hadoop fs -put - $TMP/provider_charge/provider_charge.txt
 
 hive -f $CURRENT_DIR/create_provider_charge.hql
--- solution/1-billing/etl/transform_csv.sh
+++ solution/1-billing/etl/transform_csv.sh
@@ -11,8 +11,8 @@ source $CURRENT_DIR/../../config/env.sh
 declare -a input=( "Medicare_Provider_Charge_Outpatient_APC30_CY2011_v2.csv" "Medicare_Provider_Charge_Inpatient_DRG100_FY2011.csv" )
 
 for file in $input; do
-  # Collect the columns we care about
-  csvcut -c 1,2,8,9,10,11 $DATA/summary/csv/$input > $LOCALTMP/$input
+	# Collect the columns we care about
+	csvcut -c 1,2,8,9,10,11 $DATA/summary/csv/$input > $LOCALTMP/$input
 done
 
 # Merge the DRG and APC into one CSV file
@@ -20,12 +20,16 @@ csvstack -g APC,DRG -n Type "$LOCALTMP/${input[0]}" "$LOCALTMP/${input[1]}" > $L
 
 # Clean and rename the columns, split the IPC-9 code and description into new columns
 $SED -e '1s/[()]//g' \
-  -e '1s/[[:space:]]+/_/g' \
-  -e '1s/Hospital_Referral_Region_HRR_Description/HRR/g' \
-  -e '1s/Type,APC/Type,IPC9,Procedure/p' \
-  -e 's/^(DRG|APC),([0-9]+) \- (.*)$/\1,\2,\3/p' \
-  -e 's/^(DRG|APC),"([0-9]+) \- (.+)",(.*)$/\1,\2,"\3",\4/p' \
-  $LOCALTMP/provider_charge.tmp > $LOCALTMP/provider_charge.csv
+	-e '1s/[[:space:]]+/_/g' \
+	-e '1s/Hospital_Referral_Region_HRR_Description/HRR/g' \
+	-e '1s/Type,APC/Type,IPC9,Procedure/p' \
+	-e 's/^(DRG|APC),([0-9]+) \- (.*)$/\1,\2,\3/p' \
+	-e 's/^(DRG|APC),"([0-9]+) \- (.+)",(.*)$/\1,\2,"\3",\4/p' \
+	$LOCALTMP/provider_charge.tmp > $LOCALTMP/provider_charge.csv
+
+$SED -e 's/^(.+),(.+),"(.+)",(.+),(.+),(.+),(.+),(.+)$/\1	\2	\3	\4	\5	\6	\7	\8/p' \
+		 -e 's/^(.+),(.+),(.+),(.+),(.+),(.+),(.+),(.+)$/\1	\2	\3	\4	\5	\6	\7	\8/p' \
+		 $LOCALTMP/provider_charge.csv > $LOCALTMP/provider_charge.tsv
 
 
 # Check the same number of lines are output as in the input
