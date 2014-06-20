b5a990d24fb881076bed092cb58dc4d1b3e889b8
George Agnelli <george.agnelli@gmail.com>
2014-06-20 22:25:47 +0100

    rename tools

---
+++ solution/tools/medicare/src/test/scala/org/example/MainTest.scala
@@ -0,0 +1,12 @@
+package org.example
+
+import org.scalatest._
+import Matchers._
+
+class MainTest extends FlatSpec {
+
+  "A Main" should "have a test" in {
+    "This" should be ("This")
+  }
+
+}
