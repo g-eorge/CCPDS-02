6e3095395723f7d679349595d6ed8f098504a1b8
George Agnelli <george.agnelli@gmail.com>
2014-07-01 02:47:09 +0100

    improve packaging

--- solution/tools/package.sh
+++ solution/tools/package.sh
@@ -14,15 +14,15 @@ git archive master | tar -x -C $tmp/$name
 
 solution="$tmp/$name/solution"
 
-cp $solution/1-billing/part1a/part1a.csv $solution
-cp $solution/1-billing/part1b/part1b.csv $solution
-cp $solution/1-billing/part1c/part1c.csv $solution
-cp $solution/1-billing/part1d/part1d.csv $solution
+cp $solution/1-billing/part1a/part1a.csv $solution/..
+cp $solution/1-billing/part1b/part1b.csv $solution/..
+cp $solution/1-billing/part1c/part1c.csv $solution/..
+cp $solution/1-billing/part1d/part1d.csv $solution/..
 
-cp $solution/2-similarity/part2a/part2a.csv $solution
-cp $solution/2-similarity/part2b/part2b.csv $solution
+cp $solution/2-similarity/part2a/part2a.csv $solution/..
+cp $solution/2-similarity/part2b/part2b.csv $solution/..
 
-cp $solution/3-anomaly/part3.csv $solution
+cp $solution/3-anomaly/part3.csv $solution/..
 
 cd $tmp
 tar -cjvf $name.tar.bz2 $name
